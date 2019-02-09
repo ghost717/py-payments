ee1a23f1ded752dc19d106cf50820d5fbdf9fd1a
ghost717 <dawid.zawadzki7717@gmail.com>
2019-02-09 22:07:18 +0100

    migrate

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Payment, Category
 
 # Register your models here.
+#@admin.register(Payment)
+#class PaymentAdmin(admin.ModelAdmin):
+
+admin.site.register(Payment)
+admin.site.register(Category)
--- main/models.py
+++ main/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
 # Create your models here.
-# class Category(models.Model):
-#     RODZAJE = {
-#         (0, 'Płatność'),
-#         (1, 'Płatność stała'),
-#         (2, 'Wpływ'),
-#     }
-#     cat = models.IntegerField(default=0,choices=RODZAJE)
+class Category(models.Model):
+    RODZAJE = {
+        (0, 'Płatność'),
+        (1, 'Płatność stała'),
+        (2, 'Wpływ'),
+    }
+    cat = models.IntegerField(default=0,choices=RODZAJE)
 
 class Payment(models.Model):
     name = models.CharField(max_length=128)
@@ -15,7 +15,7 @@ class Payment(models.Model):
     value = models.IntegerField(null=True, blank=True)
 
     #rel 1-1
-    #info = models.OneToOneField(Category, on_delete=models.CASCADE)
+    info = models.OneToOneField(Category, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name # return self.name
