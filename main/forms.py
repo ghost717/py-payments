198bcc5d31a2733d27bc8a764cc011d040e4a451
ghost717 <dawid.zawadzki7717@gmail.com>
2019-02-24 22:05:38 +0100

    forms updated

--- main/forms.py
+++ main/forms.py
@@ -1,13 +1,16 @@
 from django.forms import ModelForm
-from .models import Post, Images
 from django import forms
+from .models import Post, Images
 
-class PostForm(ModelForm):
+class PostForm(forms.ModelForm):
+    title = forms.CharField(max_length=128)
+    content = forms.CharField(max_length=256, label="Item Description.")
+
     class Meta:
         model = Post
         fields = ['title', 'content', 'date', 'thumbnail'] 
 
-class ImageForm(ModelForm):
+class ImageForm(forms.ModelForm):
     image = forms.ImageField(label='Image')    
     class Meta:
         model = Images
